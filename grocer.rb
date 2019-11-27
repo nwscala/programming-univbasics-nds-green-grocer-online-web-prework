d40814fd3e225f261dc553b5a1af7c8248177529
Noah Scala <tobi8912@gmail.com>
2019-11-27 20:43:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,6 +29,7 @@ def consolidate_cart(cart)
     if new_cart[index_2][:item] == new_cart[index_3][:item]
       new_cart[index_2][:count] += 1
       new_cart.delete_at(index_3)
+      index_3 += 1
     end 
     index_2 += 1 
   end
